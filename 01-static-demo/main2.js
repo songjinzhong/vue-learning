03290fd7e62c392ad4c656dda9b0e232f3ad87e8
songjinzhong <648660165@qq.com>
2017-01-19 21:54:05 +0800

    add some functions

--- 01-static-demo/main2.js
+++ 01-static-demo/main2.js
@@ -1,6 +1,6 @@
 Vue.component('vue-item', {
-  props: ['item'],
-  template: '<li>{{ item.name }}</li>'
+  props: ['list'],
+  template: '<li>{{ list.name }}</li>'
 })
 
 var app2 = new Vue({
@@ -10,6 +10,45 @@ var app2 = new Vue({
       { name : 'apple'},
       { name : 'banana'},
       { name : 'orange'}
-    ]
+    ],
+    message: "Hello world!",
+    time: Date.now(),
+    firstName: "song",
+    lastName: "jinzhong",
+    inputValue: 'song jinzhong'
+  },
+  methods: {
+    rMessage: function(){
+      return this.message.split('').reverse().join('');
+    },
+    timer: function(){
+      return this.time;
+    },
+    setName: function(){
+      this.fullName = this.inputValue;
+    }
+  },
+  computed: {
+    reverseMessage: function(){
+      return this.message.split('').reverse().join('');
+    },
+    showTime: function(){
+      return this.time;
+    },
+    fullName: {
+      get: function(){
+        return this.firstName + ' ' + this.lastName;
+      },
+      set: function(value){
+        var nameArr = value.split(' ');
+        this.firstName = nameArr[0];
+        this.lastName = nameArr[nameArr.length - 1];
+      }
+    }
+  },
+  watch: {
+    fullName: function(){
+      console.log('fullname has changed:' + this.fullName);
+    }
   }
 })
